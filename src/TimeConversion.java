fdbbaa6ca2cbaf71896fcd4c6b5e71d32a4ab55d
Abood K. AMA <abdamapro22@gmail.com>
2023-02-09 22:57:08 +0200

    time

---
+++ src/TimeConversion.java
@@ -0,0 +1,25 @@
+public class TimeConversion {
+    public static void main(String[] args) {
+
+        String s = "09:40:22AM";
+        System.out.println(timeConversion(s));;
+
+
+    }
+
+    public static String timeConversion(String s) {
+        // Write your code here
+        int hour = Integer.parseInt(s.substring(0,2));
+        String newTime = "";
+        if(s.contains("PM") && hour !=12){
+            hour+=12;
+        }
+        if(s.contains("AM") && hour == 12){
+            hour = 0;
+        }
+
+        newTime = String.format("%02d%s",hour,s.substring(2,s.length()-2));
+
+        return newTime;
+    }
+}
