cc53251eb3d3cd4db6d2287b687099552ebdb222
Abood K. AMA <abdamapro22@gmail.com>
2023-02-11 22:29:40 +0200

    SubarrayDivision

---
+++ src/DrawingBook.java
@@ -0,0 +1,29 @@
+public class DrawingBook {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int pageCount(int n, int p) {
+        // Write your code here
+        int turn = 0;
+        if (p < n/2){
+
+            for (int i = 0; i < n/2; i++) {
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+        return turn;
+    }
+
+
+}
---
+++ src/SubarrayDivision.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class SubarrayDivision {
+    public static void main(String[] args) {
+
+        List <Integer> s = new ArrayList<>();
+        s.add(2);
+        s.add(2);
+        s.add(1);
+        s.add(3);
+        s.add(2);
+
+
+
+        System.out.print(birthday(s,4,2));
+
+    }
+
+
+    public static int birthday(List<Integer> s, int d, int m) {
+        // Write your code here
+        int count = 0;
+        int sum;
+        for (int i = 0; i <= s.size()-m; i++) {
+             sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum+= s.get(i+j);
+            }
+            if (sum == d){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
