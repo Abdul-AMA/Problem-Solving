f119a7fe59a42498a71e76780bdd857a65203591
Abood K. AMA <abdamapro22@gmail.com>
2023-02-13 22:59:51 +0200

    HackerRankinaString done baby

--- src/HackerRankinaString.java
+++ src/HackerRankinaString.java
@@ -2,11 +2,19 @@ public class HackerRankinaString {
 
     public static void main(String[] args) {
 
-        System.out.print(hackerrankInString("hereiamstackerrank"));
+        System.out.println(hackerrankInString("knarrekcah"));
+        System.out.println(hackerrankInString("hackerrank"));
+        System.out.println(hackerrankInString("hackeronek"));
+        System.out.println(hackerrankInString("abcdefghijklmnopqrstuvwxyz"));
+        System.out.println(hackerrankInString("rhackerank"));
+        System.out.println(hackerrankInString("ahankercka"));
+        System.out.println(hackerrankInString("hacakaeararanaka"));
+        System.out.println(hackerrankInString("hhhhaaaaackkkkerrrrrrrrank"));
+        System.out.println(hackerrankInString("crackerhackerknar"));
+        System.out.println(hackerrankInString("hhhackkerbanker"));
+
 
     }
-
-
 
     public static String hackerrankInString(String s) {
         // Write your code here
@@ -17,10 +25,11 @@ public class HackerRankinaString {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == hackerrank.charAt(indexHack)){
                 indexHack++;
+                if (indexHack == hackerrank.length()){
+                    break;
+                }
             }
         }
-
-
 
         if (indexHack == hackerrank.length()){
             return yes;
