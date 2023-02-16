6bfdb1e906b227ab392562a7acd1ae1200344027
Abood K. AMA <abdamapro22@gmail.com>
2023-02-16 23:51:58 +0200

    CatsandaMouse

---
+++ src/CatsandaMouse.java
@@ -0,0 +1,19 @@
+public class CatsandaMouse {
+    public static void main(String[] args) {
+
+        System.out.print(catAndMouse(1,2,3));
+
+
+    }
+
+    static String catAndMouse(int x, int y, int z) {
+
+        if(Math.abs(x-z) < Math.abs(y-z)){
+            return "Cat A";
+        } else if (Math.abs(x-z) > Math.abs(y-z)) {
+            return "Cat B";
+        }else
+            return "Mouse C";
+    }
+
+}
