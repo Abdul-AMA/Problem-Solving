b77874b72abb933829c8fae78a9faf5419bb2ed8
Abood K. AMA <abdamapro22@gmail.com>
2023-02-18 00:07:16 +0200

    PickingNumbers done inshallah

--- src/PickingNumbers.java
+++ src/PickingNumbers.java
@@ -4,7 +4,7 @@ public class PickingNumbers {
 
     public static void main(String[] args) {
 
-        List <Integer> a = new ArrayList<>();
+        List<Integer> a = new ArrayList<>();
         a.add(4);
         a.add(6);
         a.add(5);
@@ -13,49 +13,35 @@ public class PickingNumbers {
         a.add(1);
         System.out.print(pickingNumbers(a));
 
-
     }
 
     public static int pickingNumbers(List<Integer> a) {
         // Write your code here
         int max = 0;
-        boolean is = true;
+        int x;
+        int y;
         int count = 0;
 
-        List <Integer>temp = new ArrayList<>();
 
         for (int i = 0; i < a.size(); i++) {
-            for (int j = 0; j <a.size(); j++) {
-
-
+            x = a.get(i);
+            for (int j = 0; j < a.size(); j++) {
+                y =  a.get(j);
+                if (x == y || x == y+1) {
+                    count++;
+                }
 
             }
+            if (count > max) {
+                max = count;
+            }
+            count = 0;
+
+
         }
-
-
-
-
-
 
         return max;
 
     }
 
 }
-//    int pickingNumbers(int a_count, int* a)
-//    {
-//        int i , j, max=0, counter = 0;
-//
-//        for(i = 0 ; i <= a_count-1 ; i++)
-//        {
-//            for(j = 0 ; j <= a_count -1 ; j++)
-//            {
-//                if(a[j] == a[i] || a[j] == a[i] +1)  {counter++;}
-//            }
-//
-//            if(max < counter){max = counter;}
-//            counter = 0;
-//        }
-//
-//        return max;
-//    }
