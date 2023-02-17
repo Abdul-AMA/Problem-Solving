b03e5c7fedf8d2b3749da0cd19eb15425500c952
Abood K. AMA <abdamapro22@gmail.com>
2023-02-17 23:41:55 +0200

    PickingNumbers not done yet

---
+++ src/PickingNumbers.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class PickingNumbers {
+
+    public static void main(String[] args) {
+
+        List <Integer> a = new ArrayList<>();
+        a.add(4);
+        a.add(6);
+        a.add(5);
+        a.add(3);
+        a.add(3);
+        a.add(1);
+        System.out.print(pickingNumbers(a));
+
+
+    }
+
+    public static int pickingNumbers(List<Integer> a) {
+        // Write your code here
+        int max = 0;
+
+        for (int i = 0; i < a.size()-1; i++) {
+            for (int j = 0; j < i; j++) {
+
+            }
+        }
+
+
+
+
+
+
+        return max;
+
+    }
+
+}
